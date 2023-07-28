84f746d09aebce73abbc26fb6e4aefd56fdbdf4d
Kimwater <iamkimwater@gmail.com>
2023-07-29 02:56:37 +0900

    feat: environment configuration

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  parser: '@typescript-eslint/parser',
+}
