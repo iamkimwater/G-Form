d842f3c93e7a9183e93ed78ea7a6e9512cc701f8
Kimwater <iamkimwater@gmail.com>
2023-07-30 16:27:27 +0900

    chore: delete unnecessary imports

--- src/features/ui/PreviewTab.tsx
+++ src/features/ui/PreviewTab.tsx
@@ -4,6 +4,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useFocusEffect } from '@react-navigation/native'
 import { setPreviewMode } from '../../store/slices/preview-slice'
 import { RootState } from '../../types/navigation-type'
+import ShortAnswerComponent from '../question/question-answer/types/short-answer-component'
+import LongAnswerComponent from '../question/question-answer/types/long-answer-component'
+import MultipleChoiceComponent from '../question/question-answer/types/multiple-choice-component'
+import CheckboxComponent from '../question/question-answer/types/checkbox-component'
 
 const PreviewTab = () => {
   const previewMode = useSelector(
@@ -26,7 +30,46 @@ const PreviewTab = () => {
 
   return (
     <View>
-      <Text>Preview</Text>
+      <View
+        style={{
+          margin: 10,
+          padding: 20,
+          borderStyle: 'solid',
+          borderWidth: 0.5,
+        }}
+      >
+        <ShortAnswerComponent />
+      </View>
+      <View
+        style={{
+          margin: 20,
+          padding: 20,
+          borderStyle: 'solid',
+          borderWidth: 0.5,
+        }}
+      >
+        <LongAnswerComponent />
+      </View>
+      <View
+        style={{
+          margin: 20,
+          padding: 20,
+          borderStyle: 'solid',
+          borderWidth: 0.5,
+        }}
+      >
+        <MultipleChoiceComponent />
+      </View>
+      <View
+        style={{
+          margin: 20,
+          padding: 20,
+          borderStyle: 'solid',
+          borderWidth: 0.5,
+        }}
+      >
+        <CheckboxComponent />
+      </View>
     </View>
   )
 }
